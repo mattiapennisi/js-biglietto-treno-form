5c9ef0f141426953eeb8900ccb583245f990422a
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-19 13:50:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Chiedo all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+
+const userKm = parseInt(prompt('Quanti chilometri vuoi percorrere?'))
+
+const userAge = parseInt(prompt('Quanti anni hai?'))
+
+console.log(userKm);
+
+console.log(userAge);
+
+// Calcolo il prezzo per chilometro 
+
+const kmPrice = (userKm * 0.21)
+
+// Applico lo sconto in base all'età dell'utente
+
+let agePrice = 0
+
+if (userAge<18) {
+    agePrice = kmPrice - (kmPrice * 20 / 100)
+} else if (userAge >= 65) {
+    agePrice = kmPrice - (kmPrice * 40 / 100)
+} else {
+    agePrice = kmPrice
+}
+
+// Applico al risultato il formato del prezzo con due decimali
+
+agePrice = agePrice.toFixed(2)
+
+// Stampo a schermo il prezzo del biglietto per l'utente
+
+alert(`Il tuo biglietto costa ${agePrice} €`)
